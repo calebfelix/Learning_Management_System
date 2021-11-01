52060f7b858292e4b18032ca3e04dc44723c9f78
Caleb Felix <calebfelix38@gmail.com>
2021-11-01 21:50:39 +0530

    Update TodoTask.js

--- models/TodoTask.js
+++ models/TodoTask.js
@@ -3,6 +3,10 @@ const todoTaskSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true
+    },
+    user_id: {
+        type: String,
+        required: true
     }
 })
 module.exports = mongoose.model('TodoTask', todoTaskSchema);
